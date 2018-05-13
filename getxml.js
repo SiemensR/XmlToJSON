5ec21139ef4f0a67774f142208be166bebaf3d4c
siemensr <priman2004@mail.ru>
2018-05-13 13:06:52 +0200

    comment added

--- getxml.js
+++ getxml.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const download = require('download');
+
+// download funcion for xml
 download('https://nebido.com/sitemap.xml', 'dist').then(() => {
     console.log('done!');
 });
