c345b95ab6cded40568c917486ab437ddfab6326
siemensr <priman2004@mail.ru>
2018-05-13 13:44:10 +0200

    created a parse function and save to json

--- getxml.js
+++ getxml.js
@@ -1,7 +1,23 @@
+// npm dependencies for xml and filesystem
 const fs = require('fs');
 const download = require('download');
-
-// download funcion for xml
+const x2j = require('xml2js');
+//Here is the source url for the xml file; function will download the xml, if it still does not exist
 download('https://nebido.com/sitemap.xml', 'dist').then(() => {
-    console.log('done!');
+  console.log('done!');
+});
+// xml will be saved in the folder dist
+var sInputFile = "dist/sitemap.xml"
+//starting of xml to json parser
+var p = new x2j.Parser();
+var sXMLData = fs.readFileSync(sInputFile, 'utf8');
+p.parseString(sXMLData, function( err, result ) {
+  var s = JSON.stringify( result, undefined, 3 );
+// console.log( "Result" + "\n", s, "\n" ); --> uncommit it, if you wanna see the result in console
+// create new json file with all contents from xml
+  fs.writeFile('urlList.json',s, function (err) {
+    if (err)
+    return console.log(err);
+    console.log('Successfully wrote file urlList.json!');
+  });
 });
